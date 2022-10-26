c5c5f3ce28b57bc63c8e7f5e783413f3b3a4da9d
Rabina Lamichhane <72934880+Rabinalamichhane@users.noreply.github.com>
2022-10-26 20:09:48 +0545

    Create dog.c
    hi dog

---
+++ dog.c
@@ -0,0 +1,1 @@
+The dog (Canis familiaris or Canis lupus familiaris ) is a domesticated descendant of the wolf. Also called the domestic dog, it is derived from an ancient, extinct wolf, and the modern wolf is the dog's nearest living relative. The dog was the first species to be domesticated, by hunter-gatherers over 15,000 years ago
