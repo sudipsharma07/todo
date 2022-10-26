1704a0c5c237ce876b73a4200122e196282cbc94
Rabina Lamichhane <72934880+Rabinalamichhane@users.noreply.github.com>
2022-10-26 21:58:27 +0545

    Create nn.c
    hii

---
+++ todo/nn.c
@@ -0,0 +1,1 @@
+The cat (Felis catus) is a domestic species of small carnivorous mammal. It is the only domesticated species in the family Felidae and is commonly referred to as the domestic cat or house cat to distinguish it from the wild members of the family. A cat can either be a house cat, a farm cat, or a feral cat; the latte
