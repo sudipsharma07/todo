5061b312ca465a40829defd8c704c1c2652d90b3
manilaghimire <93047030+manilaghimire@users.noreply.github.com>
2022-10-31 13:40:38 +0545

    Update rabina.c

--- rabina.c
+++ rabina.c
@@ -1,1 +1,4 @@
-With the help of HTML only, we can create static web pages. Hence, HTML is a markup language which is used for creating attractive web pages with the help of styling, and which looks in a nice format on a web browser. An HTML document is made of many HTML tags and each HTML tag contains different content. Let's see a simple example of HTML.
+With the help of HTML only, we can create static web pages. Hence, HTML is a markup language which is used for creating attractive web pages 
+with the help of styling, and which looks in a nice format on a web browser. An HTML document is made of many HTML tags and each HTML tag contains
+different content. Let's see a simple example of HTML.
+  i iam code 
